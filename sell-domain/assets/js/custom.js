51a5772a19bf320ba0ccd1b2cfa4a8f8a0d3fc27
Hung <hung.tg89@gmail.com>
2016-10-10 15:52:07 +0700

    Init

---
+++ sell-domain/assets/js/custom.js
@@ -0,0 +1,45 @@
+﻿
+/*=============================================================
+    Authour URI: www.binarytheme.com
+    License: Commons Attribution 3.0
+
+    http://creativecommons.org/licenses/by/3.0/
+
+    100% Free To use For Personal And Commercial Use.
+    IN EXCHANGE JUST GIVE US CREDITS AND TELL YOUR FRIENDS ABOUT US
+   
+    ========================================================  */
+
+(function ($) {
+    "use strict";
+    var mainApp = {
+        animation_fun: function () {
+
+            /*====================================
+               WOW ANIMATION SCRIPTS 
+              ======================================*/
+            new WOW().init();
+
+        },
+       
+        custom_fun:function()
+        {
+            /*====================================
+             WRITE YOUR   SCRIPTS  BELOW
+            ======================================*/
+
+
+
+
+        },
+
+    }
+   
+   
+    $(document).ready(function () {
+        mainApp.animation_fun();
+        mainApp.custom_fun();
+    });
+}(jQuery));
+
+
---
+++ sell-domain/assets/js/styleSwitcher.js
@@ -0,0 +1,58 @@
+﻿/*====================================
+      Author: speakitnow
+      Template: MERA Personal Portfolio Template
+      Version: 1.0
+  ======================================*/
+
+(function ($) {
+    "use strict";
+    var mainAppNew = {
+
+        main_fun: function () {
+
+            /*=====================================
+             THEME SWITCHER SCRIPTS 
+            ===================================*/
+            jQuery('#switch-panel').click(function () {
+                if (jQuery(this).hasClass('show-panel')) {
+                    jQuery('.switcher').css({ 'left': '-50px' });
+                    jQuery('#switch-panel').removeClass('show-panel');
+                    jQuery('#switch-panel').addClass('hide-panel');
+                } else if (jQuery(this).hasClass('hide-panel')) {
+                    jQuery('.switcher').css({ 'left': 0 });
+                    jQuery('#switch-panel').removeClass('hide-panel');
+                    jQuery('#switch-panel').addClass('show-panel');
+                }
+            });
+
+            $('#pink').click(function () {
+                $('#mainCSS').attr('href', 'assets/css/themes/pink.css');
+            });
+            $('#blue').click(function () {
+                $('#mainCSS').attr('href', 'assets/css/themes/blue.css');
+            });
+            $('#green').click(function () {
+                $('#mainCSS').attr('href', 'assets/css/themes/green.css');
+            });
+            $('#brown').click(function () {
+                $('#mainCSS').attr('href', 'assets/css/themes/brown.css');
+            });
+            $('#black').click(function () {
+                $('#mainCSS').attr('href', 'assets/css/themes/black.css');
+            });
+          
+        },
+
+        initialization: function () {
+            mainAppNew.main_fun();
+
+        }
+
+    }
+    // Initializing ///
+
+    $(document).ready(function () {
+        mainAppNew.main_fun();
+    });
+
+}(jQuery));
